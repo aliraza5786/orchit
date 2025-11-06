371ad8f0891bd2266a4504dbadf28f94be8a0d08
Ayesha Siddiqa <ayesha.s@streamedinc.com>
2025-11-06 10:02:49 +0500

    Fixed Notifications Logic

--- src/layout/mainLayout/components/NotificationBell.vue
+++ src/layout/mainLayout/components/NotificationBell.vue
@@ -1,64 +1,64 @@
 <template>
-  <div class="relative">
+  <div class="relative" ref="dropdownRef">
     <!--  Bell Icon -->
-    <button
-      @click="toggleDropdown"
-      class="relative flex items-center justify-center mt-2  rounded-full cursor-pointer"
-    >
-       <i class="fa-solid fa-bell text-primary text-[20px] font-bold"></i>
+    <button @click="toggleDropdown" class="relative flex items-center justify-center mt-2  rounded-full cursor-pointer">
+      <i class="fa-solid fa-bell text-primary text-[20px] font-bold"></i>
 
       <span v-if="unreadCount > 0"
-            class="absolute -top-1 -right-1 bg-red-500 text-white text-[10px] font-semibold rounded-full px-[5px] py-[1px]">
+        class="absolute -top-1 -right-1 bg-red-500 text-white text-[10px] font-semibold rounded-full px-[5px] py-[1px]">
         {{ unreadCount }}
       </span>
     </button>
 
     <!--  Dropdown -->
     <div v-if="isOpen"
-         class="absolute -right-14 mt-3 w-70 sm:w-96 bg-bg-lavender border-b border-border-input rounded-xl shadow-2xl overflow-hidden z-50">
+      class="absolute -right-14 mt-3 w-70 sm:w-96 bg-bg-lavender border-b border-border-input rounded-xl shadow-2xl overflow-hidden z-50">
       <!-- Header -->
       <div class="px-4 py-3 border-b border-border-input  flex items-center justify-between">
         <h3 class="text-[16px] font-bold font-manrope text-primary">Notifications</h3>
-        <button v-if="notifications.length" class="text-[12px] cursor-pointer font-bold font-manrope text-accent underline"
-                @click="markAllRead">Mark all as read</button>
+        <button v-if="notifications.length"
+          class="text-[12px] cursor-pointer font-bold font-manrope text-accent underline" @click="markAllRead">Mark all
+          as read</button>
       </div>
 
       <!-- Loading / Error -->
       <div v-if="notificationsQuery.isLoading.value" class="p-5 text-sm text-gray-500">Loading...</div>
-      <div v-else-if="notificationsQuery.isError.value" class="p-5 text-sm text-red-500">Failed to load notifications</div>
+      <div v-else-if="notificationsQuery.isError.value" class="p-5 text-sm text-red-500">Failed to load notifications
+      </div>
 
       <!-- List -->
       <div v-else-if="Object.keys(groupedNotifications || {}).length" class="max-h-96 h-full overflow-y-auto">
         <template v-for="(group, label) in groupedNotifications" :key="label">
           <div class="px-4 py-2 text-xs font-semibold text-primary uppercase">{{ label }}</div>
-
-          <div v-for="notification in group" :key="notification.id"
-               @click="openNotification(notification)"
-               class="flex gap-3 px-4 py-3 border-b border-border-input last:border-b-0  cursor-pointer transition-colors"
-               :class="{
-                 'bg-bg-body ': !notification.read && theme==='light',
-                 'hover:bg-bg-body': true
-               }">
+          <div v-for="notification in group" :key="notification.id" @click="openNotification(notification)"
+            class="flex gap-3 px-4 py-3 border-b border-border-input last:border-b-0  cursor-pointer transition-colors"
+            :class="{
+              'bg-bg-body ': !notification.read && theme === 'light',
+              'hover:bg-bg-body': true
+            }">
             <div class="flex-shrink-0">
               <div class="w-10 h-10 rounded-full flex items-center justify-center text-white font-semibold text-sm"
-                   :class="avatarColorClass(notification.actor_name || notification.title || 'U')">
+                :class="avatarColorClass(notification.actor_name || notification.title || 'U')">
                 {{ initials(notification.actor_name || notification.title || 'U') }}
               </div>
             </div>
-
-            <div class="flex-1 min-w-0">
-              <p class="text-sm truncate"
-                 :class="!notification.read ? 'font-semibold text-text-primary' : 'text-text-primary'">
-                <span v-if="notification.actor_name" class="mr-1">{{ notification.actor_name }}</span>
-                <span class="text-text-secondary text-[14px] font-normal font-manrope block">
-                  {{ notification.action || notification.title }}
-                </span>
-              </p>
-              <p class="text-xs  text-text-secondary mt-1 truncate font-manrope">{{ timeAgo(notification.created_at) }}</p>
-            </div>
-
+              <div class="flex-1 min-w-0">
+                <p class="text-sm truncate"
+                  :class="!notification.read ? 'font-semibold text-text-primary' : 'text-text-primary'">
+                  <!-- <span v-if="notification.actor_name" class="mr-1">{{ notification.actor_name }}</span> -->
+                  <span class="text-text-primary text-[14px] font-bold font-manrope block">
+                    {{ notification.action || notification.title }}
+                  </span>
+                  <span class="block my-1 overflow-hidden text-ellipsis whitespace-nowrap text-[14px] text-text-secondary font-manrope">
+                    {{  notification.body  || ""}}
+                  </span>
+                </p>
+                <p class="text-xs  text-text-secondary mt-1 truncate font-manrope">{{ timeAgo(notification.created_at)
+                  }}</p>
+              </div>
             <div v-if="!notification.read" class="w-2.5 h-2.5 bg-blue-500 rounded-full mt-1"></div>
           </div>
+
         </template>
       </div>
 
@@ -69,14 +69,35 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, computed } from "vue";
+import { ref, computed, onMounted, onBeforeUnmount } from "vue";
 import { useNotificationsQuery } from "../../../queries/useNotifications";
 import { useTheme } from "../../../composables/useTheme";
+import router from "../../../router";
 const { theme } = useTheme();
 
 const isOpen = ref(false);
 const toggleDropdown = () => (isOpen.value = !isOpen.value);
 
+// handle close dropdown when click outside
+const dropdownRef = ref<HTMLElement | null>(null);
+function handleClickOutside(event: MouseEvent) {
+  if (
+    isOpen.value &&
+    dropdownRef.value &&
+    !dropdownRef.value.contains(event.target as Node)
+  ) {
+    isOpen.value = false;
+  }
+}
+onMounted(() => {
+  document.addEventListener("click", handleClickOutside);
+});
+
+onBeforeUnmount(() => {
+  document.removeEventListener("click", handleClickOutside);
+});
+
+// notifications logic
 const { notificationsQuery, unreadCountQuery, markReadMutation, markAllReadMutation } =
   useNotificationsQuery();
 
@@ -133,6 +154,7 @@ function openNotification(notification: any) {
     markReadMutation.mutate([notification.id]);
   }
   console.log("Open notification:", notification);
+  router.push(notification.url)
 }
 
 const markAllRead = () => markAllReadMutation.mutate();
@@ -142,6 +164,7 @@ const markAllRead = () => markAllReadMutation.mutate();
 ::-webkit-scrollbar {
   width: 6px;
 }
+
 ::-webkit-scrollbar-thumb {
   background-color: rgba(100, 100, 100, 0.3);
   border-radius: 10px;
