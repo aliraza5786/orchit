c00cc1b9784bf46092fd102d065f943ebbf0af37
TanveerHassan333 <tanveerhassan4445@gmail.com>
2025-12-12 16:14:39 +0500

    new build

--- dist/assets/ConfirmDeleteModal-DVfBS5UY.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./ConfirmDeleteModal.vue_vue_type_script_setup_true_lang-DaTuBLai.js";import"./index-DM5NKZ0I.js";export{m as default};
---
+++ dist/assets/CreateTaskModal-jAMWMq3j.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./CreateTaskModal.vue_vue_type_script_setup_true_lang-B0EMVN62.js";import"./index-0D4unOmS.js";export{m as default};
