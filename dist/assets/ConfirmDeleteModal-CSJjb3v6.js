7364a4b367914b44e6133bdb791c115c870dd19c
TanveerHassan333 <tanveerhassan4445@gmail.com>
2025-12-18 17:54:22 +0500

    api url update

---
+++ dist/assets/ConfirmDeleteModal-CSJjb3v6.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./ConfirmDeleteModal.vue_vue_type_script_setup_true_lang-ByxTVZYq.js";import"./index-kby-6k01.js";export{m as default};
---
+++ dist/assets/CreateTaskModal-BtLWwdsz.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./CreateTaskModal.vue_vue_type_script_setup_true_lang-Dc8_niVU.js";import"./index-kby-6k01.js";export{m as default};
