c971e234b62a13d7f1a157708663519b301bc9ab
TanveerHassan333 <tanveerhassan4445@gmail.com>
2025-12-19 15:32:57 +0500

    latest build with auth and reactivity fixes

--- dist/assets/ConfirmDeleteModal-DUS_cZwB.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./ConfirmDeleteModal.vue_vue_type_script_setup_true_lang-CpYFnRPs.js";import"./index-CK8TR6wS.js";export{m as default};
---
+++ dist/assets/CreateTaskModal-CcYAupLL.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./CreateTaskModal.vue_vue_type_script_setup_true_lang-DcSBUdoR.js";import"./index-DNVMObI8.js";export{m as default};
