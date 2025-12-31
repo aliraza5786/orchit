2296b1b43a6b2c47b1b4e9d93320be6fc3c90416
Ayesha <ayesha.s@streamedinc.com>
2025-12-31 18:21:36 +0500

    commit delete dist

--- dist/assets/ConfirmDeleteModal-bodVdJup.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./ConfirmDeleteModal.vue_vue_type_script_setup_true_lang-BI92JcBd.js";import"./index-dgdZq2sE.js";export{m as default};
--- dist/assets/CreateTaskModal-Cwp5KIKS.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./CreateTaskModal.vue_vue_type_script_setup_true_lang-DnhSe4NW.js";import"./index-dgdZq2sE.js";export{m as default};
