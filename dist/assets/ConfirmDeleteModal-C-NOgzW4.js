4fff8e0b7c2d254c02e475f783051e437201c44d
TanveerHassan333 <tanveerhassan4445@gmail.com>
2026-01-05 18:32:31 +0500

    live branch updated build

--- dist/assets/ConfirmDeleteModal-C-NOgzW4.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./ConfirmDeleteModal.vue_vue_type_script_setup_true_lang-CcxxScxp.js";import"./index-DM4dv9p1.js";export{m as default};
--- dist/assets/CreateTaskModal-BvG_Fp3I.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./CreateTaskModal.vue_vue_type_script_setup_true_lang-C40l2xar.js";import"./index-DM4dv9p1.js";export{m as default};
