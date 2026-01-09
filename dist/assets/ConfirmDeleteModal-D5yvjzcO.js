d4ad1e6a4f40a0bd370877cded5d42f8d98d6afb
TanveerHassan333 <tanveerhassan4445@gmail.com>
2026-01-09 18:17:43 +0500

    updated build for synfusion

--- dist/assets/ConfirmDeleteModal-D5yvjzcO.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./ConfirmDeleteModal.vue_vue_type_script_setup_true_lang-Cll2TaEh.js";import"./index-j3-bp8Lo.js";export{m as default};
---
+++ dist/assets/CreateTaskModal-DkTU3SNd.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./CreateTaskModal.vue_vue_type_script_setup_true_lang-B94r1ePo.js";import"./index-LB7gJH4G.js";export{m as default};
